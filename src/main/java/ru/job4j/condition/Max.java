e8bf18d02cca71739ad295088e234b88dc95db78
Vladislav <light.inc@ya.ru>
2022-12-12 14:50:38 +0300

    4.2. Тернарное сравнение [#189]

--- src/main/java/ru/job4j/condition/Max.java
+++ src/main/java/ru/job4j/condition/Max.java
@@ -7,7 +7,7 @@ public class Max {
     }
 
     public static void main(String[] args) {
-        int rsl = max(9, 12);
-        System.out.println(rsl);
+        int value = max(9, 12);
+        System.out.println(value);
     }
 }
